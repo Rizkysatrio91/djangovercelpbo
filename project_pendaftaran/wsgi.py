5b64aa605f79e0918ce7b0fe77cdfbfe9b40ec83
Rizkysatrio91 <Rizkysatriowibowo91@gmail.com>
2025-07-18 07:12:44 +0700

    Fix: Barebones wsgi.py for Vercel handler diagnosis

--- project_pendaftaran/wsgi.py
+++ project_pendaftaran/wsgi.py
@@ -1,9 +1,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-# Hapus baris ini: from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_pendaftaran.settings')
 
 handler = get_wsgi_application()
-# Hapus baris ini: handler = WhiteNoise(handler, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'staticfiles'))
